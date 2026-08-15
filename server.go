c9a59fcd2608a649c854b5706bc84754f46a57f7
Nurul Irfan <irfanlongstride@gmail.com>
2026-08-15 13:28:01 +0700

    feat: add redis connection & /health API

--- server.go
+++ server.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type User struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+type APIResponse struct {
+	Success bool   `json:"status"`
+	Message string `json:"message"`
+}
+
+type Application struct {
+	redis *redis.Client
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +34,47 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	opt, err := redis.ParseURL("redis://localhost:6379/0")
+	if err != nil {
+		panic(err)
+	}
+
+	rdb := redis.NewClient(opt)
+
+	app := &Application{
+		redis: rdb,
+	}
+
 	http.HandleFunc("/user", getUser)
+	http.HandleFunc("/health", app.pingHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func (app *Application) pingHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), 1000*time.Millisecond)
+	defer cancel()
+
+	_, err := app.redis.Ping(ctx).Result()
+
+	if err != nil {
+		response := APIResponse{
+			Success: false,
+			Message: "redis connection error",
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response := APIResponse{
+		Success: true,
+		Message: "Ok",
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
